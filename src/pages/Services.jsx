1d72ddea08eef751b3ef05874c218198f257c878
María Ángeles Berbeira Torres <mbt24gr027@student.esgerencia.com>
2026-04-24 12:21:54 +0200

    Ejercicio 1: navegación con React Router

---
+++ src/pages/Services.jsx
@@ -0,0 +1,13 @@
+function Services() {
+  return (
+    <section className="page">
+      <h1>Servicios</h1>
+
+      <p>
+        En esta sección se incorporarán la galería interactiva y el sistema de posts dinámicos.
+      </p>
+    </section>
+  )
+}
+
+export default Services
